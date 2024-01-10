ca977ebcb7b37215a09274f4b5ce56033a282874
rouaa belhaj ali <hzoe598@gmail.com>
2024-01-10 01:05:47 +0100

    1st commit

---
+++ project/project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+#from django.conf.urls import url
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', ReactView.as_view(), name="anything"),
+]
